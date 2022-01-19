948d08a855c732c4cc3877ce2c94cc719ebeb96d
Victor Belov <belovictor@gmail.com>
2022-01-20 00:49:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# waveshare_jetson_ups
+
+It is a simple additional node for [SpotMicro ROS project](https://github.com/mike4192/spotMicro) by
+[@mike4192](https://github.com/mike4192) to monitor Waveshare Jetson UPS (B) power module and send data
+in standard ROS messages to battery topic.
+
+Launching node:
+
+`roslaunch waveshare_jetson_ups wavesharejetsonups.launch`
+
---
+++ scripts/waveshareJetsonUps.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import rospy
+from sensor_msgs.msg import BatteryState
+from ina219 import INA219
+
+
+class WaveshareJetsonUPS():
+
+    def __init__(self, design_capacity=None):
+
+        rospy.loginfo("Setting Up the Waveshare Jetson UPS node...")
+
+        # Set up and title the ros node for this code
+        rospy.init_node('waveshare_jetson_ups')
+
+        # Create publishers for commanding velocity, angle, and robot states
+        self._ros_pub_battery_state = rospy.Publisher('/battery_state', BatteryState, queue_size=5)
+        self._design_capacity = design_capacity
+        self._ina219 = INA219(i2c_bus=1, addr=0x42)
+
+        rospy.loginfo("Waveshare Jetson UPS node publishers corrrectly initialized")
+
+
+    def run(self):
+        r = rospy.Rate(1.0)
+        while not rospy.is_shutdown():
+            bus_voltage = self._ina219.getBusVoltage_V()
+            shunt_voltage = self._ina219.getShuntVoltage_mV() / 1000
+            current = self._ina219.getCurrent_mA() / 1000
+            power = self._ina219.getPower_W()
+            soc = (bus_voltage - 6) / 2.4 * 100
+            if (soc > 100):
+                soc = 100
+            if (soc < 0):
+                soc = 0
+            if current >0:
+                isCharging = True
+            else:
+                isCharging = False
+            # print("Bus voltage = {}, shunt voltage = {}, current = {}, power = {}, soc = {}, isCharging = {}".format(
+            #     bus_voltage, shunt_voltage, current, power, soc, isCharging))
+            battery_status = BatteryState()
+            battery_status.voltage = bus_voltage
+            battery_status.current = current
+            battery_status.percentage = soc
+            battery_status.location = "UPS"
+            battery_status.present = True
+            if self._design_capacity != None:
+                battery_status.design_capacity = self._design_capacity
+            # Waveshare Jetson UPS has 4 cells
+            battery_status.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LION
+            if isCharging:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_CHARGING
+            else:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+            # print(battery_status)
+            self._ros_pub_battery_state.publish(battery_status)
+            r.sleep()
+
+if __name__ == "__main__":
+    ups = WaveshareJetsonUPS(design_capacity=rospy.get_param('design_capacity', None))
+    ups.run()
